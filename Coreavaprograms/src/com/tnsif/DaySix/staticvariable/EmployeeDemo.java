46f28c240cc5b5ed607aabb0d4213abf771143c8
STUDENT <STUDENT@DESKTOP-0CD8RI5>
2025-08-20 12:22:11 +0530

    Day 6 Completed

---
+++ Coreavaprograms/src/com/tnsif/DaySix/staticvariable/EmployeeDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.DaySix.staticvariable;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		
+		// creating the first object of the class and paasing the two arg with type
+		
+		System.out.println(Employee.compName);
+		Employee e = new Employee("Adithiyaa",101);
+		System.out.println(e);
+		
+		e = new Employee("Yaswanth",102);
+		System.out.println(e);
+	}
+
+}
