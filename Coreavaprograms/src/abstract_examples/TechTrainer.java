02e54ac86b5e76ebbc1b038d2788023d7802a0fa
STUDENT <STUDENT@DESKTOP-0CD8RI5>
2025-08-20 14:05:55 +0530

    Abstract Class Examples

---
+++ Coreavaprograms/src/abstract_examples/TechTrainer.java
@@ -0,0 +1,13 @@
+package abstract_examples;
+
+public class TechTrainer extends Trainer {
+	@Override
+	public void session()
+		{
+			System.out.println("Java session Ongoing !!!! ");
+		}
+	@Override
+		public void checks() {
+			System.out.println("Assignments given from inheritance !!!! ");
+		}
+}
---
+++ Coreavaprograms/src/abstract_examples/Trainer.java
@@ -0,0 +1,12 @@
+package abstract_examples;
+
+public abstract class Trainer {
+	abstract void session();
+	abstract void checks();
+	
+	
+	public void disp() {
+		System.out.println("TNS Capgemini Trainers...");
+	}
+	}
+
