9c39a1f6532945cc49e7eccc4641d7796a4032fd
STUDENT <STUDENT@DESKTOP-0CD8RI5>
2025-08-26 11:46:34 +0530

    This program contains all core java programs

---
+++ Coreavaprograms/src/com/tnsif/dayTwelve/UncheckedException.java
@@ -0,0 +1,16 @@
+package com.tnsif.dayTwelve;
+
+//Program to demonstrate unchecked exception
+
+
+public class UncheckedException {
+	public static void main(String[] args) {
+		int intArray[];
+		//try {
+			intArray = new int[] { 11, 12, 13, 14 };
+			System.out.println(intArray[6]);
+		//} catch (ArrayIndexOutOfBoundsException e) {
+			//System.err.println("Specified index does not exist. "+e.getMessage());
+		//}
+	}
+}
